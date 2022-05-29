adfe9122dce405cde5893d28733e90788f6e807d
barretojs <barreto.carvalho.b@gmail.com>
2022-05-29 18:21:04 -0300

    feat: exercise 2.1-2

--- insertion_sorting/src/main.rs
+++ insertion_sorting/src/main.rs
@@ -3,7 +3,31 @@ fn main() {
 
     insertion_sort(&mut array);
 
-    println!("Sorted array: {:?}", array);
+    println!("Increased sorted array: {:?}", array);
+
+    array = [5, 2, 4, 6, 1, 3];
+
+    exercise212(&mut array);
+
+    println!("Decreased sorted array: {:?}", array);
+}
+
+fn exercise212(array: &mut [i32]) -> () {
+    //Rewrite the insertion-sort procedure to sort into non increasing instead of non-decreasing order
+    //Just check for array(i) < key
+
+    for j in 1..array.len() {
+        let key = array[j];
+
+        let mut i = j as i64 - 1;
+
+        while i >= 0 && array[i as usize] < key {
+            array[(i + 1) as usize] = array[i as usize];
+            i -= 1;
+        }
+
+        array[(i + 1) as usize] = key;
+    }
 }
 
 // Passes the array slice so we change the values by reference
